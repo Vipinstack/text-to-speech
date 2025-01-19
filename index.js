65f0c26c6a40d3f7e3ca5b24cd7e904d160d2919
vipins <https://gitlab.intoday.in>
2025-01-19 17:37:08 +0530

    add new modal-listing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import Storage from "./utils/Storage.js";
 import {textToSpeech} from "./methods/text-to-speech.js";
+import { VoiceListing } from "./methods/modal-listing.js";
 
 function setApiKey(apiKey) {
     Storage.apiKey = apiKey;
 }
 
-export { textToSpeech, setApiKey };
+export { textToSpeech, setApiKey, VoiceListing };
